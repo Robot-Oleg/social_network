cedece1c57733589e22aed05ea7393ec463c4aef
Robot-Oleg <kozelolezhka@gmail.com>
2021-06-03 13:12:18 +0300

    add logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            log_in @user
             redirect_to @user
+            flash[:success] = 'Congrats you create an account'
         else 
             render "new"
         end
